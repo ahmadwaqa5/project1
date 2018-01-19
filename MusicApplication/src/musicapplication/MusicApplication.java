aa0d44f2ed05724178b5ed31df5090e775f36de2
waqasahmad0 <ahmadwaqas1390@gmail.com>
2018-01-19 12:16:02 +0500

    Added some functioanlty so that we help in futre

--- MusicApplication/src/musicapplication/MusicApplication.java
+++ MusicApplication/src/musicapplication/MusicApplication.java
@@ -17,6 +17,7 @@ public class MusicApplication {
      */
     public static void main(String[] args) {
         // creating a controller object and start application
+	System.out.println("MVC based project");
         MusicAppicationController controller = new MusicAppicationController();
         controller.startApplication();
     }
